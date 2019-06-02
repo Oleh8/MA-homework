07d762377efbe12895290cd01efd523b359f7952
OLEH <oleg.busygin@gmail.com>
2019-06-02 16:36:37 +0300

    DiamondBuilder changed

--- src/hw/one/Main.java
+++ src/hw/one/Main.java
@@ -3,6 +3,14 @@ package hw.one;
 public class Main {
     public static void main(String[] args) {
 
+        DiamondBuilder.drawDiamond(7);
+
+        int a =4;
+
+//        for (int i = 0; i < 5; i++) {
+//            a++;
+//        }
+//        System.out.println(a);
 
     }
 
